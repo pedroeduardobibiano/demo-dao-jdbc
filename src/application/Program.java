ce338419ee085bbbc768306aec3eddcefc7ef6e4
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-07-02 18:33:42 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entitites.Depatment;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Depatment obj = new Depatment(1, "Books");
+        System.out.println(obj);
+
+    }
+}
