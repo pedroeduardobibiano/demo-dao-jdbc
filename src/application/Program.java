adef2fa496ac06bfe3d5d38c023eff4ff35a9b3d
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-07-02 18:44:42 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
 import model.entitites.Depatment;
+import model.entitites.Seller;
+
+import java.time.LocalDate;
+import java.util.Locale;
 
 public class Program {
     public static void main(String[] args) {
@@ -8,5 +12,7 @@ public class Program {
         Depatment obj = new Depatment(1, "Books");
         System.out.println(obj);
 
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
--- src/model/entitites/Depatment.java
+++ src/model/entitites/Depatment.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Depatment implements Serializable {
-    private static final long serialVersionUID = 1;
+    private static final long serialVersionUID = 1L;
 
 
     private Integer Id;
